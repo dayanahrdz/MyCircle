4875b2e2e2cde7ed809a227a3c31ef3114970b6f
MannyM12 <emaria@nyit.edu>
2024-12-08 15:01:44 -0500

    frontend

--- models/db.js
+++ models/db.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("Connected to MongoDB"))
+  .then(() => console.log())
   .catch((error) => console.error("MongoDB connection error:", error));
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", () => {
-  console.log("Connected to MongoDB!");
+  console.log();
 });
 
 module.exports = db;
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 // Database Connection
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connected to MongoDB"))
+  .then(() => console.log("Connected to MongoDB!"))
   .catch((error) => console.error("MongoDB connection error:", error));
 
 // Routes
